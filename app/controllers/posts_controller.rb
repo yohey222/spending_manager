03cd21bb845bbb2231a19b171de884c013f8ec4d
yohey oomori <link.yohey@gmail.com>
2022-07-17 17:48:29 +0900

    add post system, still monthly divide

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,39 @@
 class PostsController < ApplicationController
   def index
+    @now = Date.today
+    @posts = Post.where(user_id: current_user.id).order(:spend_date)
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.user_id = current_user.id
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:user_id, :title, :spend_date, :money)
   end
 end
--- db/migrate/20220717070904_create_posts.rb
+++ db/migrate/20220717070904_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
+      t.integer :user_id
       t.string :title
       t.date :spend_date
       t.integer :money
